b27a5c31f2f5b0777c9107a0d70f10ca8602f67a
Jeffrey Alvarado <jefalva10296@gmail.com>
2018-09-09 14:56:17 -0600

    Clase individuo

---
+++ Genetics/Individuo.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Genetics
+{
+    class Individuo
+    {
+        private int[] posiciones;
+        private int fitness;
+
+        public Individuo(int n)
+        {
+            posiciones = new int[n];
+            generarIndividuo();
+            calcularFitness();
+        }
+
+        private void generarIndividuo()
+        {
+            Random rnd = new Random(Guid.NewGuid().GetHashCode());
+
+            for (int i = 0; i < posiciones.Length; i++)
+            {
+                posiciones[i] = rnd.Next(1, posiciones.Length + 1);
+            }
+        }
+
+        private void calcularFitness()
+        {
+            fitness = posiciones.Distinct().Count();
+        }
+
+        public int[] getPosiciones()
+        {
+            return posiciones;
+        }
+
+        public int getFitness()
+        {
+            return fitness;
+        }
+
+        override public String ToString()
+        {
+            String salida = "Posiciones: ";
+            for(int i = 0; i < posiciones.Length; i++)
+            {
+                salida = salida + posiciones[i].ToString() + " ";
+            }
+            salida = salida + "\nFitness: " + fitness.ToString() + "\n\n";
+            return salida;
+        }
+    }
+}
--- Genetics/Program.cs
+++ Genetics/Program.cs
@@ -8,8 +8,29 @@ namespace Genetics
 {
     class Program
     {
+
+        static int n = 8;
+        static Individuo[] poblacion = new Individuo[n];
+        static Boolean found = false;
+
+        static void generarPoblacion()
+        {
+            for (int i = 0; i < n; i++)
+            {
+                poblacion[i] = new Individuo(n);
+                if (poblacion[i].getFitness() == 0)
+                {
+                    found = true;
+                }
+            }
+        }
+
         static void Main(string[] args)
         {
+            while (!found)
+            {
+                generarPoblacion();
+            }
         }
     }
 }
